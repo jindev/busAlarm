36db01b5fd75c8eed459ff699238db61e6cdcee3
jindev <jin@jinui-MacBook-Air.local>
2015-04-13 01:00:11 +0900

    메인 화면 노선 검색 버튼, 텍스트창 추가 , 메인 클릭 리스너 등록

--- app/src/main/java/com/jindev/busalarm/MainActivity.java
+++ app/src/main/java/com/jindev/busalarm/MainActivity.java
@@ -1,9 +1,13 @@
 package com.jindev.busalarm;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Vibrator;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,28 +16,32 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+      EditText busNum = (EditText) findViewById(R.id.edit_busNum);
+      Button busNumCheckBtn = (Button) findViewById(R.id.btn_busNumCheck);
+      final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+      View.OnClickListener listener = new View.OnClickListener(){
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        @Override
+        public void onClick(View v){
+          Toast.makeText(getApplicationContext(),"안녕",Toast.LENGTH_SHORT).show();
+          vibrator.vibrate(3000);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+
+      };
+
+      busNumCheckBtn.setOnClickListener(listener);
+
+
+
+
+
     }
+
+
+
 }
